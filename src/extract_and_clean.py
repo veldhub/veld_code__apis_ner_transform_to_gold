9f97e997f3359f6afdcb555116f9d8e6ead96bbc
SteffRhes <steffrhes@pm.me>
2024-06-01 19:08:47 +0200

    rerun cleaning with code formerly from executable_8, now integrated into executable_4

--- src/extract_and_clean.py
+++ src/extract_and_clean.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 import pickle
 import re
@@ -7,6 +8,9 @@ from typing import List
 
 
 OUT_JSON_UNCLEANED_PATH = os.getenv("out_json_uncleaned_path")
+OUT_JSON_CLEANED_PATH = os.getenv("out_json_cleaned_path")                                          
+OUT_JSON_CLEANED_SIMPLIFIED_PATH = os.getenv("out_json_cleaned_simplified_path")                    
+OUT_LOG_PATH = os.getenv("out_log_path")
 
 
 @dataclass
@@ -15,6 +19,7 @@ class TextEntCarrier:
     Helper class to encapsulate texts and their NER tags. All the conversion processes use this as
     transformation target.
     """
+    
     @dataclass
     class EntityMarker:
         index_beginning: int
@@ -44,6 +49,11 @@ class TextEntCarrier:
     
     def __repr__(self):
         return self.__str__()
+
+
+def print_and_log(msg):
+    print(msg)
+    logging.debug(msg)
     
 
 def convert_text_entities_tuple_format(text_entities_tuple_list):
@@ -132,17 +142,17 @@ def convert_txt_data(file_path):
         
         return mydata
     
-    print(f"convert_txt_data: {file_path}")
+    print_and_log(f"convert_txt_data: {file_path}")
     return convert_text_entities_tuple_format(read_data_from_txt(file_path))
 
 
 def convert_pickled_ner_tuples(file_path):
-    print(f"convert_pickled_ner_tuples: {file_path}")
+    print_and_log(f"convert_pickled_ner_tuples: {file_path}")
     return convert_text_entities_tuple_format(pickle.load(open(file_path, "rb")))
 
 
 def convert_pickled_ner_classes(file_path):
-    print(f"convert_pickled_ner_classes: {file_path}")
+    print_and_log(f"convert_pickled_ner_classes: {file_path}")
     return convert_text_entities_class_format(pickle.load(open(file_path, "rb")))
 
     
@@ -165,10 +175,10 @@ def evaluate_json_data(file_path, text_ent_carrier_list):
             if not found_this:
                 count_not_found += 1
         
-        print(f"count_found: {count_found}, count_not_found: {count_not_found}")
+        print_and_log(f"count_found: {count_found}, count_not_found: {count_not_found}")
 
 
-def convert_all() -> List[TextEntCarrier]:
+def extract_all() -> List[TextEntCarrier]:
     text_ent_carrier_list: List[TextEntCarrier] = []
     text_ent_carrier_list.extend(
         convert_txt_data("/veld/input/ner_apis_2019-12-03_23:32:24/corpus/trainset.txt")
@@ -251,6 +261,14 @@ def deduplicate(text_ent_carrier_list: List[TextEntCarrier]) -> List[TextEntCarr
     return text_ent_carrier_list_new
 
 
+def remove_empty(text_ent_carrier_list: List[TextEntCarrier]) -> List[TextEntCarrier]:
+    text_ent_carrier_list_new = []
+    for tec in text_ent_carrier_list:
+        if tec.entity_marker_list != []:
+            text_ent_carrier_list_new.append(tec)
+    return text_ent_carrier_list_new
+
+
 def remove_ner_noise(text_ent_carrier_list: List[TextEntCarrier]) -> List[TextEntCarrier]:
     """Removes suspected noise like 'PER-1337' from NER tags."""
     for tec in text_ent_carrier_list:
@@ -260,6 +278,124 @@ def remove_ner_noise(text_ent_carrier_list: List[TextEntCarrier]) -> List[TextEn
     return text_ent_carrier_list
 
 
+# def fix_borders(ent_list, text):
+def fix_borders(text_ent_carrier_list: List[TextEntCarrier]) -> List[TextEntCarrier]:
+    
+    def fix_char_index(text, i, i_is_start):
+        """
+        Takes the start or end index of a substring and moves it to the next valid character. Plenty
+        of data has their indices offset by a few positions, so this needs to be corrected.
+        
+        Since the potentially wrong character of a given index can be either an invalid character (
+        space, punctuation, etc.) or a valid character, this algorithm searches for the nearest
+        border of character pairs. A border is defined as a difference between character pairs in
+        one being valid and the other not.
+        
+        There is also a small bias introduced depending on the index being the start or end of a
+        substring. If the index is at the start, then the search goes first to the left and then to
+        the right, and then increases the search radius by a step of 1 in both directions, again
+        searching first to the left. If the index is at the end, this is reversed.
+        
+        Also an explicit exception rule is introduced at the end checking for periods. Since a lot
+        of periods are used as abbreviations (e.g. "N.Ö.") and this algorithm here interprets
+        periods as invalid characters, a check at the end would correct this if the period is not
+        at the end of the whole string, which would indicate a sentence boundary.
+        """
+        
+        def is_char(s):
+            return re.compile(r'[\w\d]', re.U).match(s)
+        
+        def is_not_char(s):
+            return not is_char(s)
+        
+        # The boundary detection benefits from artificial boundaries attached to the start and end
+        # so that the edge case of substrings at the start or end don't need to be handled with
+        # some dedicated logic. indices i and i_correct later on must be corrected by one step.
+        text = " " + text + " "
+        i += 1
+        # Depending on the index being at the start or end, the boundary must be flipped accordingly
+        if i_is_start:
+            is_border = lambda a, b: is_not_char(a) and is_char(b)
+            i_step = -1
+        else:
+            is_border = lambda a, b: is_char(a) and is_not_char(b)
+            i_step = 1
+        i_current = i
+        found = False
+        while not found:
+            i_a = i_current- 1
+            i_b = i_current
+            if i_a >= 0 and i_b < len(text) and is_border(text[i_a], text[i_b]):
+                i_corrected = i_b
+                found = True
+            else:
+                i_current += i_step
+                if i_step > 0:
+                    i_step += 1
+                else:
+                    i_step -= 1
+                i_step *= -1
+        # dedicated logic for the edge case of abbreviations. If a given period is not a sentence
+        # boundary, it is included.
+        if text[i_corrected] == "." and not re.compile(r'^\. *$', re.U).match(text[i_corrected:]):
+            i_corrected += 1
+        return i_corrected - 1
+    
+    def fix_parenthesis(ent, text):
+        """
+        There are some cases, where both the original data, and the correct one from the functions
+        before have lost a parenthesis (e.g. 'Bad) Ischl'). This function checks if a character next
+        to the boundaries is a parenthesis, and if it is, then checks if a non-matched counter
+        exists in the substring. If so, then it is added.
+        """
+        i_a = ent[0]
+        i_b = ent[1]
+        text_sub = text[i_a:i_b]
+        ent_new = ent
+        if i_a > 0:
+            if text[i_a - 1] == "(" and text_sub.count(")") > text_sub.count("("):
+                ent_new = [i_a - 1, i_b, ent[2]]
+        if i_b < len(text) - 1:
+            if text[i_b] == ")" and text_sub.count("(") > text_sub.count(")"):
+                ent_new = [i_a, i_b + 1, ent[2]]
+        return ent_new
+    
+    def fix_borders_main(text_ent_carrier_list):
+        text_ent_carrier_list_new = []
+        for tec in text_ent_carrier_list:
+            ent_list = [
+                [em.index_beginning, em.index_end, em.entity_type]
+                for em in tec.entity_marker_list
+            ]
+            text = tec.text_raw
+            tec_new = TextEntCarrier(text, [])
+            for ent in ent_list:
+                ent_new = [
+                    fix_char_index(text, ent[0], True),
+                    fix_char_index(text, ent[1], False),
+                    ent[2]
+                ]
+                ent_new = fix_parenthesis(ent_new, text)
+                tec_new.entity_marker_list.append(TextEntCarrier.EntityMarker(
+                    index_beginning=ent_new[0], index_end=ent_new[1], entity_type=ent_new[2]
+                ))
+                if ent != ent_new:
+                    print_and_log(
+                        f"replaced: {ent}, text: {text[ent[0]:ent[1]].__repr__()},"
+                        f" with: {ent_new}, text: {text[ent_new[0]:ent_new[1]].__repr__()}"
+                    )
+                else:
+                    print_and_log(f"nothing replaced: {ent}, {text[ent[0]:ent[1]].__repr__()}")
+                assert not (
+                    text[ent_new[0]:ent_new[1]].startswith(" ")
+                    or text[ent_new[0]:ent_new[1]].endswith(" ")
+                )
+            text_ent_carrier_list_new.append(tec_new)
+        return text_ent_carrier_list_new
+    
+    return fix_borders_main(text_ent_carrier_list)
+
+
 def write_to_file(text_ent_carrier_list: List[TextEntCarrier], output_path):
     text_ent_dict_list = [tec.to_dict() for tec in text_ent_carrier_list]
     with open(output_path, "w") as f:
@@ -267,27 +403,51 @@ def write_to_file(text_ent_carrier_list: List[TextEntCarrier], output_path):
         
 
 def main():
-    # conversion
-    print("Starting conversion.")
-    text_ent_carrier_list = convert_all()
-    print(f"Done with conversion. Length of raw converted data: {len(text_ent_carrier_list)}")
+
+    # logger setup
+    logging.basicConfig(
+        filename=OUT_LOG_PATH,
+        filemode='w',
+        level=logging.DEBUG,
+        format='%(message)s',
+    )
+    
+    # extraction
+    print_and_log("Starting conversion.")
+    text_ent_carrier_list = extract_all()
+    print_and_log(f"Done with extraction. Length of raw converted data: "
+        f"{len(text_ent_carrier_list)}"
+    )
+    
     # deduplication
-    print("Starting deduplication.")
+    print_and_log("Starting deduplication.")
     text_ent_carrier_list = deduplicate(text_ent_carrier_list)
-    print(f"Done with deduplication. Length of deduplicated data: {len(text_ent_carrier_list)}")
+    print_and_log(f"Done with deduplication. Length of deduplicated data: "
+        f"{len(text_ent_carrier_list)}"
+    )
     write_to_file(text_ent_carrier_list, OUT_JSON_UNCLEANED_PATH)
-    # removing noise
-    print("Starting removal of noise in NER tags.")
-    text_ent_carrier_list = remove_ner_noise(text_ent_carrier_list)
-    print("Done with noise removal.")
-    # deduplication of  denoised data
-    print("Starting deduplication again for denoised data.")
+    
+    # clean
+    print_and_log("Removing empty entity data items.")
+    text_ent_carrier_list = remove_empty(text_ent_carrier_list)
+    print_and_log(f"Done with removing empty entity data items. Length of cleaned data: "
+        f"{len(text_ent_carrier_list)}"
+    )
+    print_and_log("Fixing borders.")
+    text_ent_carrier_list = fix_borders(text_ent_carrier_list)
     text_ent_carrier_list = deduplicate(text_ent_carrier_list)
-    print(f"Done with deduplication.")
-    write_to_file(text_ent_carrier_list, "/veld/output/apis_ner__simplified_entities.json")
-    print(f"All Done and persisted to '/veld/output/'.")
+    write_to_file(text_ent_carrier_list, OUT_JSON_CLEANED_PATH)
+    
+    # simplify by removing ner noise
+    print_and_log("Starting removal of noise in NER tags.")
+    text_ent_carrier_list = remove_ner_noise(text_ent_carrier_list)
+    print_and_log("Done with noise removal.")
+    print_and_log("Starting deduplication again for denoised data.")
+    text_ent_carrier_list = deduplicate(text_ent_carrier_list)
+    print_and_log("Done with deduplication.")
+    write_to_file(text_ent_carrier_list, OUT_JSON_CLEANED_SIMPLIFIED_PATH)
 
 
 if __name__ == "__main__":
-    print("with __main__")
+    print_and_log("with __main__")
     main()
